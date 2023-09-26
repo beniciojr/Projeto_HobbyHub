f1e4c88e42a46dc6d73bd6345b89977419d732c7
Brunnosanttos <brunoohsanttos@gmail.com>
2023-09-26 17:01:46 -0300

    Projeto_HobbyHub

--- hobbyhub/src/pages/Cadastro/Cadastro.js
+++ hobbyhub/src/pages/Cadastro/Cadastro.js
@@ -1,38 +1,47 @@
-import React from 'react'
-import logoprojeto from '../../assets/logoprojeto.png'
-import './Cadastro.css';
+import React from 'react'; 
+import logoprojeto from '../../assets/logoprojeto.png'; 
+import './Cadastro.css'; 
 
-const Cadastro = () => {
+const Cadastro = () => { 
   return (
-    <div className='Cadcontainer0'>
-      <div className='Cadcontainer1'>
-          <h1 className='Cadcontainer1h1'>Bem-vindo!</h1>
-          <p className='Cadcontainer1p'>Comece criando um perfil e explore a ampla gama e hobbies e eventos.</p>
-          <img src={logoprojeto} alt="Logo" className="Logo" />
+    <div className='Cadcontainer0'> {/* Inicia um container principal com classe 'Cadcontainer0' */}
+      <div className='Cadcontainer1'> {/* Inicia um subcontainer com classe 'Cadcontainer1' */}
+        <h1 className='Cadcontainer1h1'>Bem-vindo!</h1> {/* Título com classe 'Cadcontainer1h1' */}
+        <p className='Cadcontainer1p'>Comece criando um perfil e explore a ampla gama de hobbies e eventos.</p> {/* Parágrafo com classe 'Cadcontainer1p' */}
+        <img src={logoprojeto} alt="Logo" className="Logo" /> {/* Imagem do logotipo com classe 'Logo' */}
       </div>
-        <div className='Cadcontainer2'>
-          <h1 className='Cadcontainer2h1'>Criar conta</h1>
-          <p className='Cadcontainer2p'>Registre-se com seu Email</p>
-            <input type="text" className='inputCad'/>
-          
-            <input type="text" className='inputCad'/>
-          
-          
-            <input type="text" className='inputCad'/>
-         
-          
-            <input type="date" className='inputCad'/>
-          
-          
-            <input type="number" className='inputCad'/>
-          
-          
-            <input type="number" className='inputCad'/>
-          
-          <button className='Cadbutton1'>Cadastre-se</button>
-        </div>
+      <div className='Cadcontainer2'> {/* Inicia outro subcontainer com classe 'Cadcontainer2' */}
+        <h1 className='Cadcontainer2h1'>Criar conta</h1> {/* Título com classe 'Cadcontainer2h1' */}
+        <p className='Cadcontainer2p'>Registre-se com seu Email</p> {/* Parágrafo com classe 'Cadcontainer2p' */}
+
+        <label>
+          <input type="email" className='inputCad' placeholder='Digite seu Email' />
+        </label>
+
+        <label>
+          <input type="text" className='inputCad' placeholder='Digite seu nome completo' />
+        </label>
+
+        <label>
+          <input type="text" className='inputCad' placeholder='Nome de usuário' />
+        </label>
+
+        <label>
+          <input type="date" className='inputCad' placeholder='Selecione a Data de Nascimento' />
+        </label>
+
+        <label>
+          <input type="text" className='inputCad' placeholder='Digite seu CPF' />
+        </label>
+
+        <label>
+          <input type="password" className='inputCad' placeholder='Digite sua Senha' />
+        </label>
+
+        <button className='Cadbutton1'>Cadastre-se</button> {/* Botão de cadastro com classe 'Cadbutton1' */}
+      </div>
     </div>
   )
 }
 
-export default Cadastro
+export default Cadastro; // Exporta o componente Cadastro para uso em outros lugares
--- hobbyhub/src/pages/Login/Login.js
+++ hobbyhub/src/pages/Login/Login.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import './Login.css';
-import logoprojeto from '../../assets/logoprojeto.png'
+import React from 'react'; 
+import './Login.css'; 
+import logoprojeto from '../../assets/logoprojeto.png'; 
 
-const Login = () => {
+const Login = () => { 
   return (
-    <div className='container0'>
-    <div className='container1'>
-      <div className='Logo_h2'>
-      <h2 className='h2'>Bem-vindo!</h2>
-      <img src={logoprojeto} alt="Logo" className="Logo" />
+    <div className='container0'> {/* Inicia um container principal com classe 'container0' */}
+      <div className='container1'> {/* Inicia um subcontainer com classe 'container1' */}
+        <div className='Logo_h2'>
+          <h2 className='h2'>Bem-vindo!</h2> {/* Título "Bem-vindo!" com classe 'h2' */}
+          <img src={logoprojeto} alt="Logo" className="Logo" /> {/* Imagem do logotipo com classe 'Logo' */}
+        </div>
       </div>
-     </div> 
-      <div className='container2'>
+      <div className='container2'> {/* Inicia outro subcontainer com classe 'container2' */}
         <div className='conteudoC2'>
-        <h1 className='h1'>Login</h1>
-        <p className='p'>Email ou CPF</p>
-        <input className='input' type="text"/>
-        <p className='p'>Senha</p>
-        <input className='input' type="password" />
-        <a href="" className='Esqueceu'>Esqueceu a senha?</a>
-        <br></br>
-        <br></br>
-        <div className='button'>
-        <button className='button1'>Login</button>
-        <br></br>
-        <button className='button1'>Criar nova conta</button>
-        </div>
+          <h1 className='h1'>Login</h1> {/* Título "Login" com classe 'h1' */}
+          
+          <label htmlFor="emailCpf" className='p'>Email ou CPF</label> 
+          <input id="emailCpf" className='input' type="text" /> 
+          
+          <label htmlFor="senha" className='p'>Senha</label> 
+          <input id="senha" className='input' type="password" /> 
+          
+          <a href="" className='Esqueceu'>Esqueceu a senha?</a> {/* Link "Esqueceu a senha?" */}
+          <br></br>
+          <br></br>
+          <div className='button'>
+            <button className='button1'>Login</button> {/* Botão de login */}
+            <br></br>
+            <button className='button1'>Criar nova conta</button> {/* Botão "Criar nova conta" */}
+          </div>
         </div>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Login
+export default Login; 
